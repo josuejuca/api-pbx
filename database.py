20bc065f0bbaaa032312e924fc8d2b478746e34f
josuejuca <josuejuca19@gmail.com>
2024-09-29 17:45:05 -0300

    API FusionPBX

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://api:Josue191203#@pbx.josuejuca.com:5432/fusionpbx"
+DATABASE_URL = "postgresql://api:Josue191203#@localhost:5432/fusionpbx"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- readme.md
+++ readme.md
@@ -41,7 +41,7 @@
    from sqlalchemy.ext.declarative import declarative_base
    from sqlalchemy.orm import sessionmaker
 
-   DATABASE_URL = "postgresql://api:Josue191203#@pbx.josuejuca.com:5432/fusionpbx"
+   DATABASE_URL = "postgresql://api:Josue191203#@localhost:5432/fusionpbx"
 
    engine = create_engine(DATABASE_URL)
    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
